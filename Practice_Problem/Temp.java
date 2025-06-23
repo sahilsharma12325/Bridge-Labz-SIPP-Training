4dd1b65f1ae27c883206c24ed244969701f350e9
sahil <sahil.sharma1_cs22@gla.ac.in>
2025-06-23 10:19:48 +0530

    Add practice program

---
+++ Practice_Problem/Temp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Temp
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float Celsius  = sc.nextFloat( );
+                float faherenhite = ( Celsius * 9/5)+32;
+		System.out.print(faherenhite);
+	}
+}
